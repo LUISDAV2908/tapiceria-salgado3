16fd34f29fa07a13a07666b5435e6741386c29f0
LUISDAV2908 <luisdavidsalgadoram@gmail.com>
2025-06-06 18:42:11 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,12 +1,12 @@
 function abrirWhatsApp(){
-	const url="https:wa.me/5212215750623?text=hola%20quiero%20cotizar,%20vi%20su%20pagina";
+	const url="https:wa.me/5212215750623?text=hola%20quiero%20cotizar,%20Vi%20su%20pagina";
 	window.open(url, "_blank");
 }
 
 function IrANosotros(){
-	window.open("QUIENES SOMOS.html", "_blank");
+	window.open("QUIENES-SOMOS.html", "_blank");
 }
 
 function IRASERVICIOS(){
-	window.open("Tapiceria Salgado.html", "_blank")
+	window.open("Tapiceria-Salgado.html", "_blank")
 }
